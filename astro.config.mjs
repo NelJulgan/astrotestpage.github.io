5d38bee90d7180cddc9e32979185172f0973b53c
NelJ <32374083+NelJulgan@users.noreply.github.com>
2023-09-14 12:35:00 +0800

    deleted .nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrotestpage.github.io',
-  output: 'static',
-  outDir: './docs',
-  build: {
-    assets: 'astro',
-  },
 });
